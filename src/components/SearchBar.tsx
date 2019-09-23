0b5066b2598d35d0de32a01a02678f089ccac258
samuel <samueldcabral@gmail.com>
2019-09-22 21:07:50 -0300

    After making the query it clears the input field. Readme updated

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -12,7 +12,8 @@ const SearchBar: React.FC = ( ) => {
     let {visibility, main, wind, name, weather} = res.data
     let {temp_min, temp_max, humidity, temp} = main
     setWeather({'visibility': visibility, 'windSpeed': wind.speed, 'name': name, 'weather': weather[0].main, 'min': temp_min, 
-                'max': temp_max, 'temp': temp, 'humidity': humidity, 'icon_id' : weather[0].icon})
+                'max': temp_max, 'temp': temp, 'humidity': humidity, 'icon_id' : weather[0].icon});
+    setSearchText("");
   }
 
   const handleChange = (event : React.ChangeEvent<HTMLInputElement>)  => {
@@ -32,7 +33,7 @@ const SearchBar: React.FC = ( ) => {
       </h1>
       <br/>
       <div className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Enter the name of the city" aria-label="Enter the name of the city" aria-describedby="button-addon2" onChange={(event) => handleChange(event)} onKeyDown={(event) => handleKeyDown(event)}/>
+        <input type="text" className="form-control" placeholder="Enter the name of the city" value={searchText} aria-label="Enter the name of the city" aria-describedby="button-addon2" onChange={(event) => handleChange(event)} onKeyDown={(event) => handleKeyDown(event)}/>
         <div className="input-group-append">
         <button className="btn btn-success" type="button" id="button-addon2" onClick={() => handleClick()}>Click me</button>
         </div>
